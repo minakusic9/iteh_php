f6b86d52b0313f2396fcd91a79479f533648d06e
minakusic9 <minakusic9@gmail.com>
2021-11-14 12:54:40 +0100

    Dodata stranica za pretragu repertoara

--- index.php
+++ index.php
@@ -2,6 +2,89 @@
     include 'header.php';
 ?>
 
+<div class="container mt-5">
+    <h1 class="text-center">Pretraga repertoara</h1>
+    <div class="row mt-4">
+        <div class="col-4">
+            <select class="form-control" id="film">
+                <option value="0">
+                    Svi filmovi
+                </option>
+            </select>
+        </div>
+        <div class="col-5">
+            <input type="text" placeholder="Pretrazi..." id='pretraga' class="form-control">
+        </div>
+        <div class="col-3">
+            <input type="date" placeholder="Datum" id='datum' class="form-control">
+        </div>
+    </div>
+    <table class="mt-3 table table-dark">
+        <thead>
+            <tr>
+                <th>Naziv filma</th>
+                <th>Cena karte</th>
+                <th>Trajanje filma</th>
+                <th>Ocena filma</th>
+                <th>Sala</th>
+                <th>Pocetak</th>
+            </tr>
+        </thead>
+    </table>
+</div>
+<script>
+    let prikazi = [];
+
+    $(document).ready(function () {
+
+        $('#film').change(function () {
+            popuniTabelu();
+        })
+
+        $('#pretraga').change(function () {
+            popuniTabelu();
+        })
+
+        $('#datum').change(function () {
+            popuniTabelu();
+        })
+        $.getJSON('./server/index.php?akcija=prikaz.read', function (res) {
+            if (!res.status) {
+                alert(res.error);
+                return;
+            }
+            prikazi = res.data;
+            popuniTabelu();
+        });
+        $.getJSON('./server/index.php?akcija=film.read', function (res) {
+            if (!res.status) {
+                alert(res.error);
+                return;
+            }
+            for (let film of res.data) {
+                $('#film').append(`
+                <option values='${film.id}'>${film.naziv}</option>
+                `)
+            }
+        })
+    })
+    function popuniTabelu() {
+        const film = Number($('#film').val());
+        const datumStr = $('#datum').val();
+        const datum = new Date(datumStr);
+        const pretraga = $('#pretraga').val();
+        const filtrirani = prikazi.filter(function (element) {
+            const elementDatum = new Date(element.datum);
+
+            return (!film || element.film.id == film)
+                && (element.film.naziv.includes(pretraga) || element.sala.naziv.includes(pretraga))
+                && (datumStr === '' || (datum.getDate() === elementDatum.getDate() && datum.getMonth() === elementDatum.getMonth() && elementDatum.getFullYear() === datum.getFullYear()))
+        })
+        for (let prikaz of filtrirani) {
+
+        }
+    }
+</script>
 <?php
     include 'footer.php';
 ?>
